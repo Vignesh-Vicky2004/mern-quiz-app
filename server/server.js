82cda17e0682389b8d4d10c297185b47a4dc9ea3
vigneshsandeep2004 <vigneshsandeep2004@gmail.com>
2024-07-09 20:15:43 +0530

    your message

--- server/server.js
+++ server/server.js
@@ -8,22 +8,20 @@ const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
 const resportsRoute = require("./routes/reportsRoute");
 
-
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", resportsRoute);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client" , "build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });   
-} 
-
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
