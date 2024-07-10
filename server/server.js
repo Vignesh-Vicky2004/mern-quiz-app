bea4e2851ac8d53d69d2faa3a4728bae8947fbaf
Vignesh <63893279+Vignesh-Vicky2004@users.noreply.github.com>
2024-07-10 12:52:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "client", "public")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
