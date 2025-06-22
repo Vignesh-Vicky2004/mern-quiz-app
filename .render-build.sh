33ff13edfa5224e896ccc67b8f3e21e07b541b19
Vignesh <63893279+Vignesh-Vicky2004@users.noreply.github.com>
2025-06-22 23:06:32 +0530

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Step 1: Install and build React frontend
+# Step 1: Build frontend
 cd client
 npm install
 npm run build
 
-# Step 2: Go to server and install backend dependencies
+# Step 2: Install backend
 cd ../server
 npm install
